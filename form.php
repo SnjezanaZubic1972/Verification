b3fd5a3a40458f0d2d1acc743e68691d106c6eb4
Snjezana Zubic <snjezana_zubic@netex.ro>
2023-08-28 21:38:20 +0200

    form.php

---
+++ form.php
@@ -0,0 +1,25 @@
+<?php
+
+if(isset($_POST['submit'])) {
+    echo "hello";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Verificate</title>
+</head>>
+<input>
+<input action="form.php" method="post">
+
+    <input type="text" placeholder="Enter name">Name</input>
+    <input type="email" placeholder="Enter email">Email</input>><br>
+    <input type="number" placeholder="Enter age">Age</input><br>
+    <input type="submit" name="submit">Submit</input>
+
+
+
+</form>
+</body>>
+</html>>
