b05bdf1a8f9464fb9f6c4e842bd91ed49f55ea44
Snjezana Zubic <snjezana_zubic@netex.ro>
2023-09-03 20:32:15 +0200

    validation form age

--- form.php
+++ form.php
@@ -22,24 +22,28 @@ if(isset($_POST['submit'])) {
             } else if (!regex.test(x)) {
                 alert("Name contains invalid characters (letters and spaces only!)");
                 return false;
-            } else {
-                return true;
             }
-        }
-    </script>
-    <script>
-        function validateFormEmail() {
-            let regex = new new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
-            let x = document.forms["Form"]["email"].value;
-            if (x == null || x === "") {
+
+
+            let reg = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+            let y = document.forms["Form"]["email"].value;
+            if (y == null || y === "") {
                 alert("Email must not be blank");
                 return false;
-            } else if (!regex.test(x)) {
+            } else if (!reg.test(y)) {
                 alert("Email contains invalid characters");
                 return false;
-            } else {
-                return true;
             }
+
+            let value = parseFloat(document.forms["Form"]["age"].value);
+            if (!value.toString().match(/^[-]?\d*\.?\d*$/)) {
+                alert("Age is positive integer");
+                return false;
+            }else if (value < 0) {
+                alert("Age is not positive integer");
+                return false;
+            }
+            return true;
         }
     </script>
 
@@ -48,7 +52,7 @@ if(isset($_POST['submit'])) {
 <form action="form.php" name="Form" onsubmit="return validateForm()" method="post">
 
     <input type="text" name="name" placeholder="Enter name">Name</input>
-    <input type="email" name="email" onsubmit="return validateFormEmail()" placeholder="Enter email">Email</input>><br>
+    <input type="email" name="email" placeholder="Enter email">Email</input>><br>
     <input type="number" name="age" placeholder="Enter age">Age</input><br>
     <input type="submit" name="submit">Submit</input>
 
